70b653c56896c33c23871723ea8925cfb7b68d31
AbdulAhad <49091109+Aadichohan@users.noreply.github.com>
2025-01-16 19:31:09 +0500

    worked on data list

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -6,11 +6,25 @@ from .Response import Response
 
 def index(request):
     # print('req ', request)
-    data = {'id': 1, 'namess': 'item.name', 'description': 'item.description', 'price': 200}
+    data = {
+            'id': 1, 
+            'namess': 
+            'item.name', 
+            'description': 'item.description', 
+            'price': 200
+        }
+    datas = [data, data]
+    # print("{")
+    # for key in data.keys():
+    #     print(f"'{key}' : '{data[key]}'")
+    # print("}")
 
+    for item in datas:
+        print(item)
+   
     # return render(request, "e_commerce/index.html")
     return Response( 
-        data    = [data, data], 
+        data    = datas, 
         status  = 200, 
         error   = {'code': 300, 'message': 'Item Created Successfully'}, 
         headers = {'X-Custom-Header': 'Item Created Successfully'}
