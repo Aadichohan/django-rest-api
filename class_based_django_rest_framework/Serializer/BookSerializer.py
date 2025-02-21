c78fcbec5b666e76c70929cfc4fa2404d4c21513
AbdulAhad <49091109+Aadichohan@users.noreply.github.com>
2025-02-21 17:02:06 +0500

    worked on foreignkey serializer

--- class_based_django_rest_framework/Serializer/BookSerializer.py
+++ class_based_django_rest_framework/Serializer/BookSerializer.py
@@ -1,16 +1,43 @@
 from rest_framework.serializers import ModelSerializer
-from  e_commerce.models import Book
+from  e_commerce.models import Book, Author, Categories
+
+class AuthorSerializer(ModelSerializer):
+    class Meta:
+        model = Author
+        fields = [
+            'id',
+            'name',
+            'last_name',
+            'age',
+            'created_at',
+        ]
+        read_only_fields = ['created_at']
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Categories
+        fields = [
+            'id',
+            'title',
+            'desc',
+            'created_at',
+        ]
+        read_only_fields = ['created_at']
+
 
 class BookSerializer(ModelSerializer):
+    author   = AuthorSerializer()
+    category = CategorySerializer()
     class Meta:
         model = Book
         fields = [
             'id',
             'title',
-            'author',
-            'category',
             'desc',
             'published_date',
             'created_at',
+            'author',
+            'category',
         ]
         read_only_fields = ['created_at']
